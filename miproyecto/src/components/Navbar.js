e5f8bb9868a0f00940d3af95e429e4bdf2b96c9b
lucianocaro3 <lucianocaro065@gmail.com>
2022-03-23 11:32:55 -0300

    desafio 2 navbar

---
+++ miproyecto/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+
+ function Navbar() {
+  return (
+    <div>
+      <h1> Sneaker World</h1>
+      <nav>
+        <a href="#"> Hombre </a>
+        <a href="#"> Mujer </a>
+        <a href="#"> Niños </a>
+        <a href="#"> Marcas </a>
+        <a href="#"> Contacto </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
